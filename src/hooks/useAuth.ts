68217d430ee077968a362858890425b24b90d64d
Omitsusan199411 <59048819+Omitsusan199411@users.noreply.github.com>
2022-06-08 23:49:20 +0900

    ログイン認証機能実装 axiosを投入。カスタムフック(useAuth)でstateを作成し、Login.tsxに渡す

---
+++ src/hooks/useAuth.ts
@@ -0,0 +1,30 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import axios from "axios";
+import { useCallback, useState } from "react";
+import { useHistory } from "react-router-dom";
+
+import { User } from "../types/api/User";
+
+// finallly()を使う場合は、tsconfigの中のesのバージョンをes2018に変更すること
+export const useAuth = () => {
+  const history = useHistory();
+  const [Loading, setLoading] = useState(false);
+
+  const login = useCallback((id: string) => {
+    setLoading(true);
+    axios
+      .get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
+      .then((res) => {
+        if (res.data) {
+          history.push("/home");
+        } else {
+          alert("ユーザーが見つかりません");
+        }
+      })
+      .catch(() => alert("ログインできません"))
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+  return { login, Loading };
+};

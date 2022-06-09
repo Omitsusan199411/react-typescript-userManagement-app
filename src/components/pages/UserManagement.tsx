bcd4dbe134b25c9671dd94d87b3644e3f04a50ae
Omitsusan199411 <59048819+Omitsusan199411@users.noreply.github.com>
2022-06-09 23:15:50 +0900

    ユーザー一覧ページ実装 UserCard.tsxでユーザーカードをコンポーネント化

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,9 +1,18 @@
 import { memo, VFC } from "react";
+import { Wrap, WrapItem } from "@chakra-ui/react";
+
+import { UserCard } from "../organisms/user/UserCard";
 
 export const UserManagement: VFC = memo(() => {
   return (
-    <>
-      <p>ユーザー管理ページ</p>
-    </>
+    <Wrap p={{ base: 4, md: 10 }}>
+      <WrapItem>
+        <UserCard
+          imageUrl="https://source.unsplash.com/random"
+          userName="やまぴー"
+          fullName="Yamashita keizou"
+        />
+      </WrapItem>
+    </Wrap>
   );
 });

7bfcb185e680b5ee1fefe34527cb7abf72ab8a8f
Omitsusan199411 <59048819+Omitsusan199411@users.noreply.github.com>
2022-06-12 17:49:27 +0900

    ユーザー管理画面にModalを実装

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,11 +1,31 @@
-import { memo, useEffect, VFC } from "react";
-import { Wrap, WrapItem, Spinner, Center } from "@chakra-ui/react";
+import { memo, useEffect, VFC, useCallback } from "react";
+import {
+  Wrap,
+  WrapItem,
+  Spinner,
+  Center,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  useDisclosure,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  Stack,
+  FormControl,
+  FormLabel,
+  Input
+} from "@chakra-ui/react";
 
 import { UserCard } from "../organisms/user/UserCard";
 import { useAllUsers } from "../../hooks/useAllUsers";
 
 export const UserManagement: VFC = memo(() => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const { getUsers, users, loading } = useAllUsers();
+
+  // propsで渡す関数は、ページが読み込まれる度に再レンダリングされるのでuseCallbackを使う
+  const onClickUser = useCallback(() => onOpen(), [onOpen]);
 
   // 初期レンダリングのときのみユーザー情報を表示させたい
   // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -24,11 +44,39 @@ export const UserManagement: VFC = memo(() => {
                 imageUrl="https://source.unsplash.com/random"
                 userName={user.username}
                 fullName={user.name}
+                onClickUser={onClickUser}
               />
             </WrapItem>
           ))}
         </Wrap>
       )}
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>ユーザー詳細</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Stack>
+              <FormControl>
+                <FormLabel>名前</FormLabel>
+                <Input value="まこと" isReadOnly />
+              </FormControl>
+              <FormControl>
+                <FormLabel>フルネーム</FormLabel>
+                <Input value="岡田　誠" isReadOnly />
+              </FormControl>
+              <FormControl>
+                <FormLabel>Mail</FormLabel>
+                <Input value="12345@example.com" isReadOnly />
+              </FormControl>
+              <FormControl>
+                <FormLabel>TEL</FormLabel>
+                <Input value="000-222-3333" isReadOnly />
+              </FormControl>
+            </Stack>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
     </>
   );
 });
